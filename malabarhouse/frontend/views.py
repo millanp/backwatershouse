2ef55c518dcd5ae0d0f25d9414969cc60fed1615
millanp <millan.philipose@gmail.com>
2015-05-23 08:11:04 -0700

    Updated the gateway (added invalid credentials thing)

--- malabarhouse/frontend/views.py
+++ malabarhouse/frontend/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http.response import HttpResponse
 # from django.http.response import HttpResponse
 
 # Create your views here.
+@login_required
 def home(request):
-    return render(request, "frontend/gateway.html", {})
+#     return render(request, "frontend/gateway.html", {})
+    return HttpResponse("OOKLE")
--- malabarhouse/malabarhouse/urls.py
+++ malabarhouse/malabarhouse/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'frontend/gateway.html'}),
     url(r'^$', include("frontend.urls")),
     # url(r'^blog/', include('blog.urls')),
-    url('^registration/', include('registration.urls')),
+    #url('^registration/', include('registration.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    
 )
