cc5a54a0262fadf06ad9e1a1bf03bff02b1eae21
millanp <millan.philipose@gmail.com>
2015-07-31 08:43:11 -0700

    spiffed up booking model
    still needs migration

--- malabarhouse/backend/models.py
+++ malabarhouse/backend/models.py
@@ -8,8 +8,18 @@ class Booking(models.Model):
         ('3','Room 3'),
         ('4','Room 4')
     )
+    EXTRAS = (
+        ('1','Extra 1'),
+        ('2','Extra 2'),
+        ('3','Extra 3'),
+    )
     guest = models.ForeignKey(User)
     arrive = models.DateField()
     leave = models.DateField()
     rooms = models.CharField(max_length=1, choices=ROOM_CHOICES)
+    extras = models.CharField(max_length=1, choices=EXTRAS)
+    approved = models.BooleanField()
+    def approve(modeladmin, request, queryset):
+        queryset.update(approved=True)
+        approve.short_description = "Approve this request"
     #     extras ...
