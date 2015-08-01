d2fa7c2e1cba737cc56017921031c7c52698413f
millanp <millan.philipose@gmail.com>
2015-08-01 09:06:53 -0700

    messed around a bit with admin
    Need to make function to convert manytomany field into string

--- malabarhouse/backend/admin.py
+++ malabarhouse/backend/admin.py
@@ -3,6 +3,8 @@ from backend.models import Booking, Room, Extra
 
 # Register your models here.
 # TODO: make bookings view prettier
-admin.site.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('guest', 'arrive', 'leave', 'rooms')
+admin.site.register(Booking, BookingAdmin)
 admin.site.register(Room)
 admin.site.register(Extra)
