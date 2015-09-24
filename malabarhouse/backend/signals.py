90902882832ff49d44ce0b436578071c78e7138a
Millan Philipose <millan.philipose@gmail.com>
2015-09-23 17:20:13 -0700

    starting to add paypal signals

---
+++ malabarhouse/backend/signals.py
@@ -0,0 +1,8 @@
+from paypal.standard.ipn.signals import valid_ipn_received
+from django.dispatch.dispatcher import receiver
+from paypal.standard.models import ST_PP_COMPLETED
+from backend.models import Booking
+@receiver(valid_ipn_received)
+def callback(ipn_obj, **kwargs):
+    if ipn_obj.payment_status == ST_PP_COMPLETED:
+        Booking.objects.filter(pk=eval(ipn_obj.custom))
