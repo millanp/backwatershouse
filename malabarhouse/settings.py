04177b76d7918c4cdfcfc2a56b0e459d69adf25d
millanp <millan.philipose@gmail.com>
2015-09-26 10:40:24 -0700

    reset migrations.

--- malabarhouse/settings.py
+++ malabarhouse/settings.py
@@ -84,8 +84,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+#     'default': 
 }
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
