36b25f8e100d7455c9b30f691c4b37122220c458
millanp <millan.philipose@gmail.com>
2015-09-26 10:18:19 -0700

    fdf.

--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
-from models import Booking, BookingForm
+from .models import Booking, BookingForm
 from django.core.urlresolvers import reverse, reverse_lazy
 # Create your views here.
 class BookingCreate(CreateView):
