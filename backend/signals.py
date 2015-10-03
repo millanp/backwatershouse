663c7354cff539723f85aa998ebc4f3f5c9f7e01
millanp <millan.philipose@gmail.com>
2015-10-03 09:49:56 -0700

    added logging to paypal signal.

--- backend/signals.py
+++ backend/signals.py
@@ -2,8 +2,11 @@ from paypal.standard.ipn.signals import valid_ipn_received
 from django.dispatch.dispatcher import receiver
 from paypal.standard.models import ST_PP_COMPLETED
 from backend.models import Booking
+import logging
 @receiver(valid_ipn_received)
 def callback(ipn_obj, **kwargs):
+    logger = logging.getLogger('testlogger')
+    logger.info('LOLOLOLOLOLOLOL LOLOLOLO LOLOLOLOLO LOLO LO LOLOLOLOLLOL')
     if ipn_obj.payment_status == ST_PP_COMPLETED:
         booking = Booking.objects.filter(pk=eval(ipn_obj.custom))
         booking.update(paid_for=True)
