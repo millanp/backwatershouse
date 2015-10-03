27e7644d677aa1287ca77dd59a6d3c9d3a81951d
millanp <millan.philipose@gmail.com>
2015-10-03 08:38:41 -0700

    added loggers to settings.

--- malabarhouse/settings.py
+++ malabarhouse/settings.py
@@ -45,7 +45,38 @@ PAYPAL_TEST = True
 SITE_URL = "https://malabarhouse.herokuapp.com"
 #SESSION_COOKIE_AGE = 30 #I should set this to a reasonable time, not 30 secs
 # Application definition
-
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
