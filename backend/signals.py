45682107d97620141ac89f1c84a19da3fae7d75c
millanp <millan.philipose@gmail.com>
2015-10-03 09:58:50 -0700

    gg.

--- backend/signals.py
+++ backend/signals.py
@@ -1,4 +1,4 @@
-from paypal.standard.ipn.signals import valid_ipn_received
+from paypal.standard.ipn.signals import valid_ipn_received, invalid_ipn_recieved
 from django.dispatch.dispatcher import receiver
 from paypal.standard.models import ST_PP_COMPLETED
 from backend.models import Booking
@@ -10,3 +10,7 @@ def callback(ipn_obj, **kwargs):
     if ipn_obj.payment_status == ST_PP_COMPLETED:
         booking = Booking.objects.filter(pk=eval(ipn_obj.custom))
         booking.update(paid_for=True)
+@receiver(invalid_ipn_recieved)
+def cal(ipn_object, **kwargs):
+    logger = logging.getLogger('testlogger')
+    logger.info('LOLOLOLOLOLOLOL LOLOLOLO LOLOLOLOLO LOLO LO LOLOLOLOLLOL')
