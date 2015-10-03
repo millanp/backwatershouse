14163a41a1693c060cfc352ef2f9bf6c4b2ca51a
millanp <millan.philipose@gmail.com>
2015-10-03 10:03:53 -0700

    ff.

--- backend/signals.py
+++ backend/signals.py
@@ -5,11 +5,14 @@ from backend.models import Booking
 import logging
 @receiver(valid_ipn_received)
 def callback(ipn_obj, **kwargs):
-    logger = logging.getLogger('testlogger')
-    logger.info('LOLOLOLOLOLOLOL LOLOLOLO LOLOLOLOLO LOLO LO LOLOLOLOLLOL')
-    if ipn_obj.payment_status == ST_PP_COMPLETED:
-        booking = Booking.objects.filter(pk=eval(ipn_obj.custom))
-        booking.update(paid_for=True)
+	
+    bookings = Booking.objects.all()
+    bookings.update(paid_for=True)
+#    logger = logging.getLogger('testlogger')
+#    logger.info('LOLOLOLOLOLOLOL LOLOLOLO LOLOLOLOLO LOLO LO LOLOLOLOLLOL')
+#    if ipn_obj.payment_status == ST_PP_COMPLETED:
+#        booking = Booking.objects.filter(pk=eval(ipn_obj.custom))
+#        booking.update(paid_for=True)
 @receiver(invalid_ipn_recieved)
 def cal(ipn_object, **kwargs):
     logger = logging.getLogger('testlogger')
