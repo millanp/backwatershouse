414cb28d57ab01176182f052cc09400b8e2a4337
millanp <millan.philipose@gmail.com>
2015-10-10 09:27:38 -0700

    Made my own view, for heaven's sake.

--- backend/views.py
+++ backend/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import Booking, BookingForm
 from django.core.urlresolvers import reverse, reverse_lazy
+import requests
 # Create your views here.
 class BookingCreate(CreateView):
     model = Booking
     form_class = BookingForm
     success_url = '/booking'
     template_name = 'frontend/booking.html'
+def paypal_processer(request):
+    r = requests.post("https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_notify-validate&", params=request.POST.dict())
+    b = Booking.objects.all()
+    b.update(paid_for=True)
+    
