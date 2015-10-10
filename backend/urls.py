b277a993a871be4834c38a9181c1e7510161081d
millanp <millan.philipose@gmail.com>
2015-10-10 15:58:36 -0700

    does the pakage work after all?

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url, include
 from backend import views
 urlpatterns = patterns('', 
     url(r'^booking-submit', views.BookingCreate.as_view(), name='booking_submit'),
-    #url(r'^paypal/', include('paypal.standard.ipn.urls'))
-    url(r'^paypal/', views.paypal_processer, name='paypal-responder'),
+    url(r'^paypal/', include('paypal.standard.ipn.urls'))
+#     url(r'^paypal/', views.paypal_processer, name='paypal-responder'),
 )
