5554ea771250e61d2f50add14aa2d78a03cdec4f
millanp <millan.philipose@gmail.com>
2015-10-10 09:35:05 -0700

    Made it send an email on paypal response

--- backend/views.py
+++ backend/views.py
@@ -3,6 +3,7 @@ from django.views.generic.edit import CreateView
 from .models import Booking, BookingForm
 from django.core.urlresolvers import reverse, reverse_lazy
 import requests
+from django.core.mail import send_mail
 # Create your views here.
 class BookingCreate(CreateView):
     model = Booking
@@ -10,7 +11,9 @@ class BookingCreate(CreateView):
     success_url = '/booking'
     template_name = 'frontend/booking.html'
 def paypal_processer(request):
-    r = requests.post("https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_notify-validate&", params=request.POST.dict())
+    send_mail('ff', 'ff', 'donotreply@mala.com', ['millan.philipose@gmail.com'])
     b = Booking.objects.all()
     b.update(paid_for=True)
+    r = requests.post("https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_notify-validate&", params=request.POST.dict())
+    
     
