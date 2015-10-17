a9ce1787aaff4f081b438e8273e7cd701483e03f
Millan Philipose <millan.philipose@gmail.com>
2015-10-17 16:16:57 -0700

    added password reset page

--- malabarhouse/urls.py
+++ malabarhouse/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import django.contrib.auth.views as djviews
 urlpatterns = patterns('',
     # Examples:
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {}),
-
+    url('^', include('django.contrib.auth.urls')),
     # url(r'^blog/', include('blog.urls')),
     #url('^registration/', include('registration.urls')),
     url(r'^admin/', include(admin.site.urls)),
