4c89131fce9f89285b96a844a0eab34825fcfbb3
millanp <millan.philipose@gmail.com>
2015-10-18 17:58:20 -0700

    logout view added

--- malabarhouse/urls.py
+++ malabarhouse/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.contrib.auth import urls
+from django.contrib.auth import urls, views
 urlpatterns = patterns('',
     # Examples:
+    url('^accounts/logout/', views.logout_then_login, name='logout'),
     url('^accounts/', include(urls)),
     # url(r'^blog/', include('blog.urls')),
     #url('^registration/', include('registration.urls')),
