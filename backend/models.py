1e74f0ea912447d6ea7dc053b8c0757e3a8201b8
Millan Philipose <millan.philipose@gmail.com>
2015-10-25 16:49:00 -0700

    connected the create_calendars to post_init room

--- backend/models.py
+++ backend/models.py
@@ -11,35 +11,39 @@ from oauth2client.client import SignedJwtAssertionCredentials
 from googleapiclient.discovery import build
 from httplib2 import Http
 from backend.helpers import cal_api
+from django.db.models.signals import post_save, post_init
 # Create your models here.
 class Room(models.Model):
     number = models.PositiveSmallIntegerField()
     blurb = models.TextField(max_length=700)
+    request_cal_id = models.TextField()
+    booking_cal_id = models.TextField()
     def __init__(self, *args, **kwargs):
         
         models.Model.__init__(self, *args, **kwargs)
     def __str__(self):
         return "Room "+str(self.number)
-    def create_calendars(self):
-        calapi = cal_api()
-        newcals = [
-            {'summary':str(self),},
-            {'summary':'Requests for'+str(self)}
-        ]
-        calresources = [calapi.calendars().insert(body=newcal).execute() for newcal in newcals]
-        aclrule = {
-            'role':'owner',
-            'scope':{
-                'type':'user',
-                'value':'millan.philipose@gmail.com'
-            }
-        }
-        x = [calapi.acl().insert(calendarId=calresource['id'], body=aclrule).execute() for calresource in calresources]
     def get_booking_calendar_id(self):
         calendarapi = cal_api()
         cal_list = calendarapi.calendarList().list(minAccessRole="writer").execute()
         for cal in cal_list:
             print cal['summary']
+def create_calendars(sender, instance, **kwargs):
+    calapi = cal_api()
+    newcals = [
+        {'summary':str(instance),},
+        {'summary':'Requests for '+str(instance)}
+    ]
+    calresources = [calapi.calendars().insert(body=newcal).execute() for newcal in newcals]
+    aclrule = {
+        'role':'owner',
+        'scope':{
+            'type':'user',
+            'value':'millan.philipose@gmail.com'
+        }
+    }
+    x = [calapi.acl().insert(calendarId=calresource['id'], body=aclrule).execute() for calresource in calresources]
+post_init.connect(create_calendars, sender=Room)
 class Booking(models.Model):
     guest = models.ForeignKey(User)
     arrive = models.DateField()
