14dbe6f738dd6f84c4f4ae0a4b04da429f622012
Millan Philipose <millan.philipose@gmail.com>
2015-11-06 17:02:35 -0800

    wiring ajax for booking form

--- frontend/views.py
+++ frontend/views.py
@@ -14,23 +14,18 @@ class AjaxableResponseMixin(object):
     """
     def form_invalid(self, form):
         response = super(AjaxableResponseMixin, self).form_invalid(form)
-        if self.request.is_ajax():
-            return JsonResponse(form.errors, status=400)
-        else:
-            return response
+        return JsonResponse(form.errors, status=400)
 
     def form_valid(self, form):
         # We make sure to call the parent's form_valid() method because
         # it might do some processing (in the case of CreateView, it will
         # call form.save() for example).
         response = super(AjaxableResponseMixin, self).form_valid(form)
-        if self.request.is_ajax():
-            data = {
-                'pk': self.object.pk,
-            }
-            return JsonResponse(data)
-        else:
-            return response
+        data = {
+            'pk': self.object.pk,
+        }
+        return JsonResponse(data)
+
 class BookingCreate(AjaxableResponseMixin, CreateView, LoginRequiredMixin):
     form_class = BookingForm
     success_url = '/booking'
