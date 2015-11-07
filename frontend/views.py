2b1898b898e186dfff1a3a59fc7d1cbebea45194
Millan Philipose <millan.philipose@gmail.com>
2015-11-06 17:07:40 -0800

    wiring ajax for booking form

--- frontend/views.py
+++ frontend/views.py
@@ -6,27 +6,7 @@ from braces.views import LoginRequiredMixin
 from django.views.generic.edit import CreateView
 from malabarhouse import settings
 from django.http.response import JsonResponse
-
-class AjaxableResponseMixin(object):
-    """
-    Mixin to add AJAX support to a form.
-    Must be used with an object-based FormView (e.g. CreateView)
-    """
-    def form_invalid(self, form):
-        response = super(AjaxableResponseMixin, self).form_invalid(form)
-        return JsonResponse(form.errors, status=400)
-
-    def form_valid(self, form):
-        # We make sure to call the parent's form_valid() method because
-        # it might do some processing (in the case of CreateView, it will
-        # call form.save() for example).
-        response = super(AjaxableResponseMixin, self).form_valid(form)
-        data = {
-            'pk': self.object.pk,
-        }
-        return JsonResponse(data)
-
-class BookingCreate(AjaxableResponseMixin, CreateView, LoginRequiredMixin):
+class BookingCreate(CreateView, LoginRequiredMixin):
     form_class = BookingForm
     success_url = '/booking'
     template_name = 'frontend/booking.html'
@@ -34,7 +14,8 @@ class BookingCreate(AjaxableResponseMixin, CreateView, LoginRequiredMixin):
         obj = form.save(commit=False)
         obj.guest = self.request.user
         obj.save()
-        return super(CreateView, self).form_valid(form)
+        super(CreateView, self).form_valid(form)
+        return JsonResponse({"FOO":"FFFFFS"})
 # Create your views here.
 class TemplateViewPlus(LoginRequiredMixin, TemplateView):
     template_name = "frontend/home.html"
