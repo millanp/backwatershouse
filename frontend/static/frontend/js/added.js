d9604b0c50fff7df4c31dc3d10dbec00387e007f
millanp <millan.philipose@gmail.com>
2015-11-07 20:10:54 -0800

    piping errors through

--- frontend/static/frontend/js/added.js
+++ frontend/static/frontend/js/added.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 alert(jqXHR.responseText);
-                alert(document.getElementById('formWrapper').innerHTML);
+                $('#formWrapper').html(jqXHR.responseText);
             }
         });
     });
