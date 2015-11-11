c1f0eade223341f40ca34138ec2a829cced66b82
Millan Philipose <millan.philipose@gmail.com>
2015-11-11 12:32:01 -0800

    using unique email form

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,7 +3,6 @@ from frontend import views
 from backend.models import BookingForm, Booking, Room
 from django.views.generic.edit import FormView
 urlpatterns = patterns('',
-    url(r'^registration/', include('registration.urls')),
     url(r'^booking/', views.BookingCreate.as_view(), name='booking'),
     url(r'^my-visits/', views.requestsView, name='requests'),
     url(r'^rooms/', views.TemplateViewPlus.as_view(template_name='frontend/rooms.html', 
--- malabarhouse/urls.py
+++ malabarhouse/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth import urls, views
+from registration.forms import RegistrationFormUniqueEmail
+from registration.views import RegistrationView
 urlpatterns = patterns('',
     # Examples:
     url('^accounts/logout/', views.logout_then_login, name='logout'),
     url('^accounts/', include(urls)),
+    url(r'^registration/register/', RegistrationView.as_view(form_class=RegistrationFormUniqueEmail)),
+    url(r'^registration/', include('registration.urls')),
     # url(r'^blog/', include('blog.urls')),
     #url('^registration/', include('registration.urls')),
     url(r'^admin/', include(admin.site.urls)),
