ab6bd6a58073e0f1fc9c7bc5a7d0366b3e04895f
Millan Philipose <millan.philipose@gmail.com>
2015-11-11 12:52:14 -0800

    matching only exactly registration register to url

--- malabarhouse/urls.py
+++ malabarhouse/urls.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 from django.contrib.auth import urls, views
 from registration.forms import RegistrationFormUniqueEmail
 from registration.views import RegistrationView
+class RegistrationViewUniqueEmail(RegistrationView):
+    form_class=RegistrationFormUniqueEmail
 urlpatterns = patterns('',
     # Examples:
     url('^accounts/logout/', views.logout_then_login, name='logout'),
     url('^accounts/', include(urls)),
-    url(r'^registration/register/$', RegistrationView.as_view(form_class=RegistrationFormUniqueEmail), name="registration_register"),
+    url(r'^registration/register/$', RegistrationViewUniqueEmail.as_view(), name="myregistration_register"),
     url(r'^registration/', include('registration.urls')),
     # url(r'^blog/', include('blog.urls')),
     #url('^registration/', include('registration.urls')),
