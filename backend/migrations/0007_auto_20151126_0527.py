8ddca9deabb09a0aa57777a3485f310855b8882e
millanp <millan.philipose@gmail.com>
2015-11-26 11:03:10 +0530

    asfd

--- backend/migrations/0007_auto_20151126_0527.py
+++ backend/migrations/0007_auto_20151126_0527.py
@@ -16,16 +16,4 @@ class Migration(migrations.Migration):
             model_name='booking',
             name='request_event_id',
         ),
-        migrations.AddField(
-            model_name='booking',
-            name='request_event_ids',
-            field=django.contrib.postgres.fields.hstore.HStoreField(default={}),
-            preserve_default=False,
-        ),
-        migrations.AlterField(
-            model_name='booking',
-            name='booking_event_id',
-            field=django.contrib.postgres.fields.hstore.HStoreField(default={}),
-            preserve_default=False,
-        ),
     ]
