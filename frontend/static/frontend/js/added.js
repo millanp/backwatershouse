a5edd3e9ef25cce4aa7d0b03211550530cf63d50
millanp <millan.philipose@gmail.com>
2015-11-28 21:11:59 +0530

    commence floppification

--- frontend/static/frontend/js/added.js
+++ frontend/static/frontend/js/added.js
@@ -9,12 +9,12 @@ $(document).ready(function() {
             success: function(data) {
                 alert(JSON.stringify(data));
 		alert("FAO");
-		          $('#formWrapper').html(data + "<p>Booking complete</p>");
+		          $('#formWrapper').html(data + "<p class='successMessage'>Booking complete</p>");
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 alert(jqXHR.responseText);
 		alert("FOO");
-                $('#formWrapper').html(jqXHR.responseText + "<p>Error</p>");
+                $('#formWrapper').html(jqXHR.responseText);
 		alert("FOOF");
             }
         });
