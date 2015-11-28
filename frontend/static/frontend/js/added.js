f66de83f8389f18f36b61e9e3e3875575bf5e75e
millanp <millan.philipose@gmail.com>
2015-11-28 19:31:52 +0530

    display html on success

--- frontend/static/frontend/js/added.js
+++ frontend/static/frontend/js/added.js
@@ -9,12 +9,12 @@ $(document).ready(function() {
             success: function(data) {
                 alert(JSON.stringify(data));
 		alert("FAO");
-		          $('#formWrapper').html(data);
+		          $('#formWrapper').html(data + "<p>Booking complete</p>");
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 alert(jqXHR.responseText);
 		alert("FOO");
-                $('#formWrapper').html(jqXHR.responseText);
+                $('#formWrapper').html(jqXHR.responseText + "<p>Error</p>");
 		alert("FOOF");
             }
         });
