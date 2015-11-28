da29ce0c062a6cd2ec1be36b7af1e9a734fcc12e
millanp <millan.philipose@gmail.com>
2015-11-28 22:10:53 +0530

    flopp

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.forms.models import ModelForm
+from floppyforms import ModelForm
 from paypal.standard.forms import PayPalPaymentsForm
 from malabarhouse import settings
 from backend import helpers
