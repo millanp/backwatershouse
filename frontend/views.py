ae452606c343a8238ed8374dbeb3666633984fea
millanp <millan.philipose@gmail.com>
2015-11-30 09:40:57 +0530

    asdf

--- frontend/views.py
+++ frontend/views.py
@@ -12,7 +12,7 @@ def google_calendar_url():
     suffix = r"ctz=America%2FLos_Angeles"
     color = 0
     for room in Room.objects.all():
-        thiscal = "src="+room.request_cal_id.strip()+"&color=%23"+colors.COLORS[color]+"&src="+room.booking_cal_id.strip()+"&color=%23"+colors.COLORS[color+1]
+        thiscal = "src="+room.request_cal_id.strip()+"&color=%23"+colors.COLORS[color]+"&src="+room.booking_cal_id.strip()+"&color=%23"+colors.COLORS[color+1]+"&"
         prefix += thiscal
         color += 2
     prefix += suffix
