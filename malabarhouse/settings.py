3f79ef7047a545cfe679b90b2afd4fe7bb1079c9
millanp <millan.philipose@gmail.com>
2016-01-19 07:17:52 -0800

    Switching to sublime

--- malabarhouse/settings.py
+++ malabarhouse/settings.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-
+# Switched to Sublime Text
 import os
 
 import dj_database_url
