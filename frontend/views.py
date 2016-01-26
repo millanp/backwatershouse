74bc16f9848fb0614bf16c651b4e021f38937a77
millanp <millan.philipose@gmail.com>
2016-01-25 19:31:42 -0800

    Indent

--- frontend/views.py
+++ frontend/views.py
@@ -17,9 +17,9 @@ def google_calendar_url():
     color = 0
     for room in Room.objects.all():
         thiscal = ("src="+room.request_cal_id.strip()
-        "&color=%23"+colors.COLORS[color]
-        "&src="+room.booking_cal_id.strip()
-        "&color=%23"+colors.COLORS[color+1]+"&")
+                   "&color=%23"+colors.COLORS[color]
+                   "&src="+room.booking_cal_id.strip()
+                   "&color=%23"+colors.COLORS[color+1]+"&")
         prefix += thiscal
         color += 2
     prefix += suffix
