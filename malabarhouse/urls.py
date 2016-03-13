c24cba7334eb0ed74ca35d0857b57e112c3846c6
millanp <millan.philipose@gmail.com>
2016-03-13 10:53:26 -0700

    switch url import statements

--- malabarhouse/urls.py
+++ malabarhouse/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.contrib import auth
-from django.contrib.auth import views as authurls
+from django.contrib.auth import urls as authurls
+from django.contrib.auth import views as authviews
 from dappr import urls as dapprurls
 
 admin.site.site_header = "Malabar House Administration"
 urlpatterns = patterns('',
     # Examples:
-    url('^accounts/logout/', views.logout_then_login, name='logout'),
+    url('^accounts/logout/', authviews.logout_then_login, name='logout'),
     url('^accounts/', include(authurls)),
     url('^r/', include(dapprurls)),
     url(r'^admin/', include(admin.site.urls)),
