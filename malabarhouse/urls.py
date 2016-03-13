cda325716dc5a05365c467dfe06d8231e8fb868b
millanp <millan.philipose@gmail.com>
2016-03-13 10:03:35 -0700

    switch imports and urls to dappr

--- malabarhouse/urls.py
+++ malabarhouse/urls.py
@@ -1,20 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.contrib.auth import urls, views
+from django.contrib import auth
 from registration.forms import RegistrationFormUniqueEmail
 from registration.views import RegistrationView
+import dappr
 class RegistrationViewUniqueEmail(RegistrationView):
     form_class=RegistrationFormUniqueEmail
 
 admin.site.site_header = "Malabar House Administration"
 urlpatterns = patterns('',
     # Examples:
-    url('^accounts/logout/', views.logout_then_login, name='logout'),
-    url('^accounts/', include(urls)),
-    url(r'^registration/register/$', RegistrationView.as_view(custom_form_class=RegistrationFormUniqueEmail), name="myregistration_register"),
-    url(r'^registration/', include('registration.urls')),
-    # url(r'^blog/', include('blog.urls')),
-    url('^registration/', include('registration.urls')),
+    url('^accounts/logout/', auth.views.logout_then_login, name='logout'),
+    url('^accounts/', include(auth.urls)),
+    url('^r/', include(dappr.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^backend/', include("backend.urls")),
     url(r'^', include("frontend.urls")),
