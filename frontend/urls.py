147c91e5b91625cef84f2381f3b2c5ac37718b6e
millanp <millan.philipose@gmail.com>
2016-04-10 19:59:13 -0700

    Create class-based MyVisitsView

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,9 +4,7 @@ from backend.models import BookingForm, Booking, Room
 from django.views.generic.edit import FormView
 urlpatterns = patterns('',
     url(r'^booking/', views.BookingCreate.as_view(), name='booking'),
-    url(r'^my-visits/', views.InnerPageView.as_view(template_name='frontend/requests.html',
-                                                    context={'bookings': Booking.objects.filter(guest=request.user)},
-                                                   ), name='requests'),
+    url(r'^my-visits/', views.MyVisitsView.as_view(), name='requests'),
     url(r'^rooms/', views.InnerPageView.as_view(template_name='frontend/rooms.html', 
                                                    context={'rooms':Room.objects.all()}),
         name='rooms'),
