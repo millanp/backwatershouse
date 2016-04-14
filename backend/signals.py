7ae273ee6b5c1cfbf6db3d35cd1b4b9b1f361165
millanp <millan.philipose@gmail.com>
2016-04-14 12:36:13 -0700

    tweak invalid ipn tester

--- backend/signals.py
+++ backend/signals.py
@@ -3,6 +3,8 @@ from django.dispatch.dispatcher import receiver
 from paypal.standard.models import ST_PP_COMPLETED
 from backend.models import Booking
 import logging
+
+
 @receiver(valid_ipn_received)
 def callback(ipn_obj, **kwargs):
     bookings = Booking.objects.all()
@@ -10,7 +12,9 @@ def callback(ipn_obj, **kwargs):
     if ipn_obj.payment_status == ST_PP_COMPLETED:
         booking = Booking.objects.filter(pk=eval(ipn_obj.custom))
         booking.update(paid_for=True)
+
+
 @receiver(invalid_ipn_received)
 def cal(ipn_object, **kwargs):
     bookings = Booking.objects.all()
-    bookings.update(paid_for=True)
+    bookings.update(approval_state=Booking.FINALIZED_PAID)
