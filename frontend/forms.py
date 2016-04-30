0c179b47e91cd212d52ddda4f596a036c57406aa
millanp <millan.philipose@gmail.com>
2016-04-30 11:26:16 -0700

    add forms.py

---
+++ frontend/forms.py
@@ -0,0 +1,16 @@
+from dappr.forms import RegistrationForm
+
+
+class PlaceholdersInsteadOfLabelsMixin(object):
+    def __init__(self, *args, **kwargs):
+        super(PlaceholdersInsteadOfLabelsMixin, self).__init__(*args, **kwargs)
+        for field_name in self.fields:
+            field = self.fields.get(field_name)
+            if field:
+                if type(field.widget) in (forms.TextInput, forms.DateInput):
+                    field.widget = forms.TextInput(attrs={'placeholder': field.label})
+                    field.label = None
+
+
+class PrettyRegistrationForm(PlaceholdersInsteadOfLabelsMixin, RegistrationForm):
+	pass
