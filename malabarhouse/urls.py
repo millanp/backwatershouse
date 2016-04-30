86470cd14cb38d2d82aafa9842a62fbd6e724096
millanp <millan.philipose@gmail.com>
2016-04-30 11:17:31 -0700

    try to use pretty registration form

--- malabarhouse/urls.py
+++ malabarhouse/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.contrib.auth import urls as authurls
 from django.contrib.auth import views as authviews
 from dappr import urls as dapprurls
+from frontend import forms
 
 admin.site.site_header = "%s Administration" % (settings.VENUE_NAME)
 
@@ -14,6 +15,7 @@ base_login_context = {
 urlpatterns = patterns('',
     # Examples:
     url('^accounts/logout/', authviews.logout_then_login, name='logout'),
+    url('^accounts/login/', authviews.login, {'authentication_form': forms.PrettyRegistrationForm}),
     url('^accounts/', include(authurls), {"extra_context":base_login_context}),
     url('^r/', include(dapprurls)),
     url(r'^admin/', include(admin.site.urls)),
