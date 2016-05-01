4c97283979ecba9397d18c6d48774aee84c96611
millanp <millan.philipose@gmail.com>
2016-05-01 08:42:32 -0700

    use pretty registration form

--- frontend/forms.py
+++ frontend/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm
-from braces.forms import Success
 from django import forms
 from dappr.forms import RegistrationForm
+
 
 class PlaceholdersInsteadOfLabelsMixin(object):
     def __init__(self, *args, **kwargs):
