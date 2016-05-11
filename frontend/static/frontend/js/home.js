0e915497573a8058b46074b7e76959a73f6ad933
Millan Philipose <millan.philipose@gmail.com>
2016-05-10 19:16:18 -0700

    use home.js

---
+++ frontend/static/frontend/js/home.js
@@ -0,0 +1,12 @@
+window.setTimeout(function() {
+	changeImage
+}, 6 * 1000);
+
+var i = 0;
+function changeImage() {
+	$('#header').css('background-image', IMAGE_URLS[i]);
+	i++;
+	if (i === IMAGE_URLS.length) {
+		i = 0;
+	}
+}
