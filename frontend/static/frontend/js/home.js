ceec237e4f4f3036b2920ca3178e2908c7e9db46
Millan Philipose <millan.philipose@gmail.com>
2016-05-11 07:02:14 -0700

    preload images

--- frontend/static/frontend/js/home.js
+++ frontend/static/frontend/js/home.js
@@ -1,6 +1,14 @@
 window.setInterval(changeImage, 6 * 1000);
 
 var i = 0;
+
+function preloadImages() {
+	for (var i = IMAGE_URLS.length - 1; i >= 0; i--) {
+		var i = new Image();
+		i.src = IMAGE_URLS[i]
+	}
+}()
+
 function changeImage() {
 	$('#header').css('background-image', "url(" + IMAGE_URLS[i] + ")");
 	i++;
