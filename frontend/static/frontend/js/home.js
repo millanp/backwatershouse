f53ae098930f7f6f0e96197022ba292dc87930d9
Millan Philipose <millan.philipose@gmail.com>
2016-05-10 19:22:18 -0700

    use home.js

--- frontend/static/frontend/js/home.js
+++ frontend/static/frontend/js/home.js
@@ -2,7 +2,7 @@ window.setTimeout(changeImage, 6 * 1000);
 
 var i = 0;
 function changeImage() {
-	$('#header').css('background-image', IMAGE_URLS[i]);
+	$('#header').css('background-image', "url(" + IMAGE_URLS[i] + ")");
 	i++;
 	if (i === IMAGE_URLS.length) {
 		i = 0;
