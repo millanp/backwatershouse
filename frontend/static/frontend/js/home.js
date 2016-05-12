13df9c49090bc3d90db8f7e9a5436093afa1cb36
Millan Philipose <millan.philipose@gmail.com>
2016-05-11 20:07:28 -0700

    add console.logs

--- frontend/static/frontend/js/home.js
+++ frontend/static/frontend/js/home.js
@@ -3,6 +3,7 @@ window.setInterval(changeImage, 6 * 1000);
 var i = 0;
 
 function preloadImages() {
+	console.log("preloading images")
 	for (var i = IMAGE_URLS.length - 1; i >= 0; i--) {
 		var i = new Image();
 		i.src = IMAGE_URLS[i]
