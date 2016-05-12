c0d79d1ade34f1c84b345ca1759a64eeea4ecac8
Millan Philipose <millan.philipose@gmail.com>
2016-05-11 20:17:48 -0700

    add console.logs

--- frontend/static/frontend/js/home.js
+++ frontend/static/frontend/js/home.js
@@ -2,13 +2,13 @@ window.setInterval(changeImage, 6 * 1000);
 
 var i = 0;
 
-function preloadImages() {
+(function preloadImages() {
 	console.log("preloading images")
 	for (var i = IMAGE_URLS.length - 1; i >= 0; i--) {
 		var i = new Image();
 		i.src = IMAGE_URLS[i]
 	}
-}()
+})()
 
 function changeImage() {
 	$('#header').css('background-image', "url(" + IMAGE_URLS[i] + ")");
